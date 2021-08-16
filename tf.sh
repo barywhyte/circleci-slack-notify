adcfdec1011a5c6c4e440c6893e4be6ce53fe634
Seun Ore <seunsamore@gmail.com>
2021-08-16 11:30:35 +0100

    adding slack notification

---
+++ tf.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Changes found";
+
+   curl -X POST -H 'Content-type: application/json' --data "{'text':'Hello world'}" https://hooks.slack.com/services/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
+   #echo $MESSAGE
+esac
