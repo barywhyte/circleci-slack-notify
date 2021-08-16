f18990df2229b91e46cf4efbd30132e18825b3aa
Seun Ore <seunsamore@gmail.com>
2021-08-16 11:41:44 +0100

    removed exposed webhook

--- tf.sh
+++ tf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Changes found";
 
-   curl -X POST -H 'Content-type: application/json' --data "{'text':'Hello world'}" https://hooks.slack.com/services/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
+   curl -X POST -H 'Content-type: application/json' --data "{'text':'Hello world'}" ${SLACK_WEBHOOK_URL}
    #echo $MESSAGE
 esac
